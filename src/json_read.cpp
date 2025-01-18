2df9275a795278d7e92be0a27585766ba2618477
Daniele <Iximiel@users.noreply.github.com>
2025-01-18 23:42:18 +0100

    my crappy parser actually parses an array

--- src/json_read.cpp
+++ src/json_read.cpp
@@ -2,6 +2,7 @@
 // This code is licensed under MIT license (see LICENSE for details)
 #include "json_read.hpp"
 #include "utils.hpp"
+#include <cctype>
 #include <iostream>
 #include <regex>
 
@@ -28,10 +29,33 @@ namespace djson {
 
   char spaceEater (std::istream &in) {
     char c;
-    while ((c = in.get ()) == ' ' || c == '\t' || c == '\n' || c == '\r') {
-    }
+    do {
+      c = in.get ();
+    } while (std::isspace (static_cast<unsigned char> (c)));
     return c;
   }
+
+  void ltrim (std::string &s) {
+    s.erase (
+      s.begin (), std::find_if (s.begin (), s.end (), [] (unsigned char ch) {
+        return !std::isspace (ch);
+      }));
+  }
+
+  void rtrim (std::string &s) {
+    s.erase (
+      std::find_if (
+        s.rbegin (), s.rend (),
+        [] (unsigned char ch) { return !std::isspace (ch); })
+        .base (),
+      s.end ());
+  }
+
+  void trim (std::string &s) {
+    ltrim (s);
+    rtrim (s);
+  }
+
   std::optional<Object> read (std::istream &in) {
     if (auto myobj = internal::readObject (in); myobj.has_value ()) {
       return std::move (*myobj);
@@ -41,12 +65,37 @@ namespace djson {
   }
   namespace internal {
 
+    std::optional<Node> strtonode (std::string rawValue) {
+      if (std::regex_match (rawValue, numberRegex)) {
+        return std::stod (rawValue);
+      } else if (std::regex_match (rawValue, boolRegex)) {
+        // return std::regex_match (rawValue, trueRegex)  is not ok:
+        // beacause there can be a typo on the false
+        if (std::regex_match (rawValue, trueRegex)) {
+          return true;
+        } else if (std::regex_match (rawValue, falseRegex)) {
+          return false;
+        } else {
+          return std::nullopt;
+        }
+      } else if (std::smatch words;
+                 std::regex_match (rawValue, words, stringRegex)) {
+        auto t = words[0].str ();
+
+        return t.substr (1, t.size () - 2);
+      } else if (std::regex_match (rawValue, nullRegex)) {
+        return Null{};
+      } else {
+        return std::nullopt;
+      }
+    }
     std::optional<Object> readObject (std::istream &in) {
       Object obj;
       bracketsValidator validator;
 
       if (char c = spaceEater (in); c != '{') {
         // cannot opend the parentesis
+        std::cerr << "cannot open the parentesis" << std::endl;
         return std::nullopt;
       } else {
         validator.open (c);
@@ -83,28 +132,16 @@ namespace djson {
             return std::nullopt;
           }
         }
+        trim (rawValue);
         if (std::regex_match (rawValue, valueRegex)) {
-          if (std::regex_match (rawValue, numberRegex)) {
-            obj[key] = std::stod (rawValue);
-          } else if (std::regex_match (rawValue, boolRegex)) {
-            // obj[key] = std::regex_match (rawValue, trueRegex)  is not ok:
-            // beacause there can be a typo on the false
-            if (std::regex_match (rawValue, trueRegex)) {
-              obj[key] = true;
-            } else if (std::regex_match (rawValue, falseRegex)) {
-              obj[key] = false;
-            } else {
-              return std::nullopt;
-            }
-          } else if (std::regex_match (rawValue, stringRegex)) {
-            std::smatch words;
-            std::regex_search (rawValue, words, stringRegex);
-            auto t = words[0].str ();
-
-            obj[key] = t.substr (1, t.size () - 2);
-          } else if (std::regex_match (rawValue, nullRegex)) {
-            obj[key] = Null{};
+          if (auto node = strtonode (rawValue); node.has_value ()) {
+            obj[key] = std::move (*node);
+          } else {
+            std::cerr << "cannot read value \"" << rawValue << "\" for key "
+                      << key << std::endl;
+            return std::nullopt;
           }
+
         } else {
           // it could be an array or another object
           // go back to when weran gerline
@@ -133,9 +170,81 @@ namespace djson {
           return obj;
         } else if (c != ',') {
           std::cerr << "cannot find the end of the object" << std::endl;
+          return std::nullopt;
         }
       }
     }
-    std::optional<Array> readArray (std::istream &in) { return std::nullopt; }
+
+    std::optional<Array> readArray (std::istream &in) {
+      Array arr;
+
+      bracketsValidator validator;
+
+      if (char c = spaceEater (in); c != '[') {
+        // cannot open the parentesis
+        std::cerr << "cannot open the parentesis" << std::endl;
+        return std::nullopt;
+      } else {
+        validator.open (c);
+      }
+      while (true) {
+        std::string rawValue;
+        auto redo = in.tellg ();
+        // gets until the comma
+        /// @todo: please no comma in the value
+        std::getline (in, rawValue, ',');
+        in.unget ();
+        if (char c = in.peek (); c != ',') {
+          // no comma found, go back and go for the parentesis
+          in.seekg (redo);
+          std::getline (in, rawValue, ']');
+          in.unget ();
+          if (char c = in.peek (); c != ']') {
+            std::cerr << "cannot find the \"}\" after value " << rawValue
+                      << std::endl;
+            return std::nullopt;
+          }
+        }
+        trim (rawValue);
+        if (std::regex_match (rawValue, valueRegex)) {
+          if (auto node = strtonode (rawValue); node.has_value ()) {
+            arr.emplace_back (std::move (*node));
+          } else {
+            std::cerr << "cannot read value \"" << rawValue << "\""
+                      << std::endl;
+            return std::nullopt;
+          }
+        } else {
+          // it could be another array or an object
+          // go back to when we ran gerline
+          in.seekg (redo);
+          char c = spaceEater (in);
+          in.unget ();
+          if (c == '[') {
+            if (auto myarray = internal::readArray (in); myarray.has_value ()) {
+              arr.emplace_back (std::move (*myarray));
+            } else {
+              std::cerr << "cannot read array " << std::endl;
+              return std::nullopt;
+            }
+          } else if (c == '{') {
+            if (auto myobj = internal::readObject (in); myobj.has_value ()) {
+              arr.emplace_back (std::move (*myobj));
+            } else {
+              std::cerr << "cannot read Object" << std::endl;
+              return std::nullopt;
+            }
+          }
+        }
+        // now we get again until the next comma or the end of the object
+        if (char c = spaceEater (in); c == ']') {
+          validator.close (c);
+          return arr;
+        } else if (c != ',') {
+          std::cerr << "cannot find the end of the array" << std::endl;
+          return std::nullopt;
+        }
+      }
+    }
   } // namespace internal
 } // namespace djson
--- tests/test_reader.cpp
+++ tests/test_reader.cpp
@@ -9,13 +9,16 @@
 TEST_CASE ("Reading an Object", "[read]") {
   std::stringstream ss (
     "\t\r\n "
-    R"(  {"num":1,"bool":false,"string":"hello","thenull":null})");
+    R"(  {"num":1, "bool":false ,"string":"hello","thenull":null,
+    "actuallyastring":"true" }
+    )");
   auto x = djson::read (ss);
   REQUIRE (x.has_value ());
   REQUIRE (std::get<djson::Number> (x->at ("num")) == Catch::Approx (1));
   REQUIRE (std::get<djson::Boolean> (x->at ("bool")) == false);
   REQUIRE (std::get<djson::String> (x->at ("string")) == "hello");
   REQUIRE (std::get<djson::Null> (x->at ("thenull")) == djson::Null{});
+  REQUIRE (std::get<djson::String> (x->at ("actuallyastring")) == "true");
 }
 
 TEST_CASE ("Reading an Object within an object", "[read]") {
@@ -34,4 +37,21 @@ TEST_CASE ("Reading an Object within an object", "[read]") {
   REQUIRE (std::get<djson::Boolean> (obj.at ("bool")) == false);
   REQUIRE (std::get<djson::String> (obj.at ("string")) == "hello");
   REQUIRE (std::get<djson::Null> (obj.at ("thenull")) == djson::Null{});
+}
+
+TEST_CASE ("Reading an Array within an object", "[read]") {
+  std::stringstream ss (
+
+    R"(  {"arr":[false, "ciao" ,
+    null ,3, {"in":"side"}]})");
+  auto x = djson::read (ss);
+  REQUIRE (x.has_value ());
+  auto &arr = std::get<djson::Array> (x->at ("arr"));
+  REQUIRE (std::get<djson::Boolean> (arr[0]) == false);
+  REQUIRE (std::get<djson::String> (arr[1]) == "ciao");
+  REQUIRE (std::get<djson::Null> (arr[2]) == djson::Null{});
+  REQUIRE (std::get<djson::Number> (arr[3]) == Catch::Approx (3));
+  REQUIRE (
+    std::get<djson::String> (std::get<djson::Object> (arr[4]).at ("in")) ==
+    "side");
 }
