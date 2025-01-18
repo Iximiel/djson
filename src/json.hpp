152a3ea6204c3dd395a3f07cc59ab6791712d982
Daniele <Iximiel@users.noreply.github.com>
2025-01-18 14:14:40 +0100

    adding some comments

--- src/json.hpp
+++ src/json.hpp
@@ -10,6 +10,9 @@
 #include <vector>
 
 namespace djson {
+  // As now the library does not support deleting things from the Objects, since
+  // is more thinked as a layer between the program and the json files
+
   using Number = double;
   using String = std::string;
   using Boolean = bool;
@@ -19,12 +22,16 @@ namespace djson {
   };
   struct Array;
   class Object;
+  /// A Node may be a number, a string, a boolean, a null, an array or an object
   using Node = std::variant<Number, String, Boolean, Null, Array, Object>;
   // I do not 100% like this
+  /// A ordered list enclosed by square brackets
   struct Array : public std::vector<Node> {};
-
+  /// @brief A object enclosed by curly brackets (that contains named values)
   class Object {
+    // I want to keep the order or the keys
     std::vector<std::string> orderedKeys{};
+    // I do not want do write a find function
     std::map<std::string, Node, std::less<>> objects{};
 
   public:
