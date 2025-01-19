661788ef55a1ff0417deaba3ab633fd4455d392c
Daniele <Iximiel@users.noreply.github.com>
2025-01-19 18:55:12 +0100

    Writing examples

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,18 +1,3 @@
-# DJSON a json library made for learning the joy of parsing
-
-> The 'D' is silent
-
-My take on a json library, for fun and learing 
-
-This is not complete, the parser I think is kinda meh, but it is an exercise for uisng the `<variant>` and try to use `<optional>` for avoiding throws.
-
-I will add some options like the possibility of setting the lib as a shared object and the CI
-
-Maybe I should try to look for more tests to make the parser better
-
-## Usage
-
-```C++
 #include "djson/json_read.hpp"
 #include "djson/json_write.hpp"
 #include <fstream>
@@ -36,7 +21,7 @@ int main () {
     djson::Number value;
     theObj.get ("number", value);
     std::cout << "number: " << value << std::endl;
-    // or this will likel became an optional return
+    // or
     value = theObj.get<djson::Number> ("number");
     std::cout << "number: " << value << std::endl;
     // or
@@ -45,14 +30,3 @@ int main () {
     std::cout << "failed to open " << filename << std::endl;
   }
 }
-```
-
-```Cmake
-find_package(Djson REQUIRED)
-add_executable(main main.cpp)
-
-target_link_libraries(
-  main
-  PRIVATE Djson::Djson)
-
-```
